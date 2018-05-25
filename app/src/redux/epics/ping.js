7aba588d497313142fd3590b473f187e95374d7b
김현석 <dragmove@naver.com>
2018-05-25 14:15:45 +0900

    feat: Add comments

--- app/src/redux/epics/ping.js
+++ app/src/redux/epics/ping.js
@@ -3,8 +3,11 @@ import { PING } from '../actions/actionTypes';
 import { pong } from '../actions/ping';
 
 // set redux-observable epic for async
-export const pingEpic = action$ =>
-    action$
-        .ofType(PING)
-        .delay(1500)
-        .mapTo(pong());
+export const pingEpic = (action$, store) => {
+  // console.log('store has getState(), dispatch() methods. but, do not use store.dispatch in epic');
+
+  return action$
+    .ofType(PING)
+    .delay(1500)
+    .mapTo(pong());
+};
--- app/src/redux/epics/users.js
+++ app/src/redux/epics/users.js
@@ -3,7 +3,10 @@ import { FETCH_USER, FETCH_USER_FULFILLED } from '../actions/actionTypes';
 import { fetchUser, fetchUserFulfilled } from '../actions/users';
 import { ajax } from 'rxjs/Observable/dom/ajax';
 
-export const fetchUserEpic = action$ =>
-  action$.ofType(FETCH_USER).mergeMap(action => {
+export const fetchUserEpic = (action$, store) => {
+  // console.log('store has getState(), dispatch() methods. but, do not use store.dispatch in epic');
+
+  return action$.ofType(FETCH_USER).mergeMap(action => {
     return ajax.getJSON(`https://api.github.com/users/${action.payload}`).map(response => fetchUserFulfilled(response));
   });
+};
